e4e1e624dd3f706a242a3509d2a877e021835b81
ecosimon <liu_simon_eco@yahoo.com>
2018-05-23 11:01:03 -0400

    changes

--- wilsite/settings.py
+++ wilsite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_u#zs+9f@qt1@%h3m+kuq)q+^@hsys7&!%$mjqz8ndrv4#a$^n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
